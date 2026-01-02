0d8927811d0091304a64c6e880aeac45046654ed
LakshmiAnitha-23 <lakshmianitha23@gmail.com>
2026-01-02 03:23:48 -0800

    mapping

---
+++ mappingException/mappingException/src/main/java/com/example/mappingException/MappingExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.mappingException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingExceptionApplication.class, args);
+	}
+
+}
